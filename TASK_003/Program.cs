178147d55315562ef1db1255a9900cd91bf741a3
Alina Lavrik <alina_voro@mail.ru>
2022-09-26 21:15:25 +0300

    Add Task 003

---
+++ TASK_003/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+4  -> 24
+5  -> 120
+*/
+
+Console.WriteLine("Введи число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Sum(N)}");
+
+int Sum(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        sum = i * sum;
+    }
+    return sum;
+}
+
