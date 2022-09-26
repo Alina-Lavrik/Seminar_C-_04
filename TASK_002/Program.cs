71aecb94722e6857eb49aeb61378efaf7eaad322
Alina Lavrik <alina_voro@mail.ru>
2022-09-26 21:00:05 +0300

    Add Task 002

---
+++ TASK_002/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5  */
+
+
+Console.WriteLine("Введи число N: ");
+string N = Console.ReadLine()!;
+Console.WriteLine($"Количество цифр в числе {N} равно {Counter(N)}");
+
+int Counter(string number)
+{
+    int sum = 0;
+    for(int i=1; i <= N.Length; i++)
+    {
+        sum = i;
+    }
+    return sum;
+}
