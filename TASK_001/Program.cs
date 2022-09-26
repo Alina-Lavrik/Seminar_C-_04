d70e75751f4e2c1836e61157cfa2d85281fda010
Alina Lavrik <alina_voro@mail.ru>
2022-09-26 20:37:01 +0300

    Add Task_001

---
+++ TASK_001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введи число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {GetSum(A)}");
+
+int GetSum(int limit)
+{
+    int sum = 0;
+    for(int i=1; i <= limit; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
